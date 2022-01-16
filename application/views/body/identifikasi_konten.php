3d5a6cf2b35fe7bc6a97709a160197cbd0f80abe
muzakkialfarisi <muzakkialfarisi10071999@gmail.com>
2022-01-16 19:17:49 +0700

    deskripsi materu 4, 5

--- application/views/body/identifikasi_konten.php
+++ application/views/body/identifikasi_konten.php
@@ -3,13 +3,16 @@
     <div class="bodyhead"> </div>
 </div>
 
-<video id="myVideo" width="320" height="176" class="d-none">
-  <source src="<?= base_url()?>indopride/indopride4(konten).mp4" type="video/mp4">
-</video>
+<div class="m-3 text-white">
+  <p> 
+      Berita Yang Benar Biasanya Ditulis
+      Secara Santun, Kontennya Jelas Dan Mudah Untuk Dimengerti Oleh Para Pembaca, Ini
+      Juga Menjadi Sebuah Gambaran Apabila Konten Berita Tidak Jelas Cara Menulis Dan
+      Menyampaikannya Ke Masyarakat Dapat Dipastikan Berita Tersebut Tidak Dibuat Oleh Orang Yang Bertanggung Jawab Dan Dapat Dipastikan Berita Tersebut Palsu, Sehingga Masyarakat Perlu Menghindari Konten Yang Demikian.
+  </p>
+</div>
 
-<div class="d-grid gap-2 m-3">
-<button type="button" onclick="playmateri1()" class="btn btn-light rounded-pill mb-2">Putar Suara Materi</button>
+<div class="d-grid gap-2 m-3 pt-5">
   <a href="<?= site_url('Welcome/materi_pembelajaran')?>" class="btn btn-light rounded-pill mb-2" type="button">Memilih Materi Pembelajaran Lain</a>
   <a href="<?= site_url('Welcome/')?>" class="btn btn-light rounded-pill mb-2" type="button">Kembali Ke Halaman Utama</a>
-
 </div>
--- application/views/body/identifikasi_sumber_berita.php
+++ application/views/body/identifikasi_sumber_berita.php
@@ -3,13 +3,14 @@
     <div class="bodyhead"> </div>
 </div>
 
-<video id="myVideo" width="320" height="176" class="d-none">
-  <source src="<?= base_url()?>indopride/indopride3(sumber berita).mp4" type="video/mp4">
-</video>
+<div class="m-3 text-white">
+  <p> 
+    Sumber Berita Adalah Penangungjawab Adanya
+    Berita Yang Disebarkan. Ini Menjadi Persoalan Siapa Yang Membuat Berita, Apakah Alasannya Karena Ingin Terkenal Dan Iseng Dalam Membuat Berita. Sehingga Berita Dengan Mudah Untuk Beredar Dan Menjadikan Berita Tersebut Tersebar Ke Masyarakat, Lalu Masyarakat Menyikapi Dan Menjadi Sebuah Keresahan. Namun Masyarakat Perlu Melihat Secara Jelas Sumber Berita Yang Ada.
+  </p>
+</div>
 
-<div class="d-grid gap-2 m-3">
-<button type="button" onclick="playmateri1()" class="btn btn-light rounded-pill mb-2">Putar Suara Materi</button>
+<div class="d-grid gap-2 m-3 pt-5">
   <a href="<?= site_url('Welcome/materi_pembelajaran')?>" class="btn btn-light rounded-pill mb-2" type="button">Memilih Materi Pembelajaran Lain</a>
   <a href="<?= site_url('Welcome/')?>" class="btn btn-light rounded-pill mb-2" type="button">Kembali Ke Halaman Utama</a>
-
 </div>
