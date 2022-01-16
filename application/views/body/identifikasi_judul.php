925e8573cc1a2acaa8598abe66b41b998602da78
anggaagustira <anggaagustira@gmail.com>
2022-01-16 19:57:01 +0700

    edit view 1,2,3

--- application/views/body/identifikasi_judul.php
+++ application/views/body/identifikasi_judul.php
@@ -3,12 +3,8 @@
     <div class="bodyhead"> </div>
 </div>
 
-<video id="myVideo" width="320" height="176" class="d-none">
-  <source src="<?= base_url()?>indopride/indopride5(judul).mp4" type="video/mp4">
-</video>
-
 <div class="d-grid gap-2 m-3">
-<button type="button" onclick="playmateri1()" class="btn btn-light rounded-pill mb-2">Putar Suara Materi</button>
+  <p class="text-white mb-3" >Judul Yang Memiliki Kesan Mengandung Perbuatan Tindakan Kejahatan Atau Bersifat Menghasut Biasanya Merupakan Tanda Hoaks, Karena Judul Yang Baik Itu Dibuat Apa Adanya Berdasarkan Peristiwa Yang Terjadi Bukan Malah Membuat Masyarakat Ikut Memberikan Dukungan Atau Simpatinya. Judul Yang Bersifat Provokatif Akan Membuat Pembacanya Merasa Dipengaruhi Dan Merasa Diajak Turut Serta Dalam Suasana Atau Keadaan Yang Terdapat Dalam Isi Berita Atau Informasi Yang Disampaikan Oleh Penulis, Hal Ini Menjadikan Kondisi Tidak Netral.</p>
   <a href="<?= site_url('Welcome/materi_pembelajaran')?>" class="btn btn-light rounded-pill mb-2" type="button">Memilih Materi Pembelajaran Lain</a>
   <a href="<?= site_url('Welcome/')?>" class="btn btn-light rounded-pill mb-2" type="button">Kembali Ke Halaman Utama</a>
 
